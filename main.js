3c0bc275ba02f1c08bc545e9c47e0ffdb79e1bbb
YURY <zionlenuz1@gmail.com>
2018-05-10 14:39:52 -0500

    Barra Lateral puro Javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+
+	const btnToggle = document.querySelector('.toggle-btn');
+
+	btnToggle.addEventListener('click', function() {
+		// alert("asd")
+		document.getElementById('sidebar').classList.toggle('active');
+	})
+
+
